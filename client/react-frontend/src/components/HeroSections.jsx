3494cdb5ec26de995a3780d55c2ca2066c1fda8b
elsilv <elsi.silvennoinen@gmail.com>
2024-10-24 12:07:30 +0300

    Fix sign in link

--- client/react-frontend/src/components/HeroSections.jsx
+++ client/react-frontend/src/components/HeroSections.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       <div className="hero-content">
         <h1>Welcome to Meowtivate</h1>
         <p>"If at first you don’t succeed, knock everything off the table and try again."</p>
-        <Link to="/signup" className="btn">Get Started</Link>
+        <Link to="/signin" className="btn">Get Started</Link>
       </div>
     </section>
   );
