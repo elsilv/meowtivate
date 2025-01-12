9044d6b4a4e37a63e380860e53ae2426f1ee0377
elsilv <elsi.silvennoinen@gmail.com>
2025-01-12 15:52:37 +0200

    Add GitHub link to footer

--- client/react-frontend/src/components/Footer.jsx
+++ client/react-frontend/src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/index.css';
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer" id="contact">
+      <p>
+        <a href="https://github.com/elsilv/meowtivate"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         View on GitHub <FaGithub/>
+        </a>
+      </p>
       <p>&copy; 2024 Meowtivate. All rights reserved.</p>
+
     </footer>
   );
 };
